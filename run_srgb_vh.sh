f4c832feebc1083bded2cd1213a5f3a0bcb9622a
hiro <jinguang.tong@anu.edu.au>
2024-06-11 22:06:37 +1000

    update

--- run_srgb_vh.sh
+++ run_srgb_vh.sh
@@ -5,9 +5,9 @@ for scene in ${scenes[@]}; do
     # CUDA_VISIBLE_DEVICES='1' python render.py -m output/${scene}_pbr_gs_linear --brdf_dim 0 --sh_degree -1 --brdf_mode envmap --brdf_env 512 --shading gs --linear;
     # CUDA_VISIBLE_DEVICES='1' python metrics.py -m output/${scene}_pbr_gs_linear) &
     (
-        python train.py -s data/refnerf/${scene} --eval -m output/refnerf/${scene}/pbr_gs_mtx -w --brdf_dim 0 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512 --shading gs
-        python render.py -m output/refnerf/${scene}/pbr_gs_mtx --brdf_dim 0 --sh_degree -1 --brdf_mode envmap --brdf_env 512 --shading gs
-        python metrics.py -m output/refnerf/${scene}/pbr_gs_mtx
+        python train.py -s data/refnerf/${scene} --eval -m output/refnerf/pbr_gs_vh/${scene} -w --brdf_dim 0 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512 --shading gs
+        python render.py -m output/refnerf/pbr_gs_vh/${scene} --brdf_dim 0 --sh_degree -1 --brdf_mode envmap --brdf_env 512 --shading gs
+        python metrics.py -m output/refnerf/pbr_gs_vh/${scene}
     )
     wait
     # (
